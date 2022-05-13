62fbe2c689560a5f1b25c421ccfb418136746730
vagrant <vagrant@vagrant.vm>
2022-05-13 12:50:49 +0000

    Ajout

--- sb-project-master/src/main/java/hello/GreetingController.java
+++ sb-project-master/src/main/java/hello/GreetingController.java
@@ -4,6 +4,13 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 public class GreetingController {
@@ -19,6 +26,23 @@ public class GreetingController {
 
     @RequestMapping("/movies")
     public String movies() {
-        return "movies";
-    }
+        List<String> listMovies=new ArrayList<String>();
+        String sqlSelectAllMovies ="Select * from movies";
+        String connectionUrl="jdbc:mysql://127.0.0.1:3306/movies?serverTimezone=UTC";
+        try(Connection conn = DriverManager.getConnection(connectionUrl,"root","admin");
+            PreparedStatement ps = conn.prepareStatement(sqlSelectAllMovies);
+            ResultSet rs=ps.executeQuery()){
+                while (rs.next()){
+                    String title=rs.getString("title");
+                    listMovies.add(title);
+                }
+
+            }catch(SQLException throwables){
+                throwables.printStackTrace();
+            }
+        return listMovies.toString();
+
+            
+        }
+    
 }
